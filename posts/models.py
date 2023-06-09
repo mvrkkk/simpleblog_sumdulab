a52ba2da47f084c04d1ac556f0c3fd9d7ccbad84
markofchainz <stakan.mark@pdffiller.team>
2023-06-10 01:57:29 +0300

    feature fix

--- posts/models.py
+++ posts/models.py
@@ -11,11 +11,10 @@ class User(AbstractUser):
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_post")
     title = models.CharField(max_length=165)
-    body = HTMLField()
+    text = HTMLField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
